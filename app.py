98317fbccea82f68ce4e9314a3e8a891459625aa
Adam Pacholski <adam.pacholski@rover.com>
2026-01-31 15:39:29 -0500

    Dockerize

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 # app.py
+import os
+
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
+APP_NAME = os.environ.get("APP_NAME", "echo-flask")
 
 
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def all_routes(path):
     return jsonify({
+        'app_name': APP_NAME,
+        'host': request.host,
         'json': request.get_json(silent=True),
         'data': request.form,
         'query': request.args,
@@ -17,5 +24,4 @@ def all_routes(path):
 
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
